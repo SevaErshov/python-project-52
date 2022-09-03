a6a3ff3a335057a1e724cd8f4a19ca4de43056bb
SevaErsh <erschidvs@yandex.ru>
2022-09-03 22:49:09 +0300

    edit views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Я фанат Михаила Лабковского и сериала \'Доктор Хаус\'!")
+    return HttpResponse("<h1 style=\" font-size: 200px;\">Я фанат Михаила Лабковского и сериала \'Доктор Хаус\'!</h1>")
