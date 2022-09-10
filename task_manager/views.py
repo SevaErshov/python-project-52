94529529d4b9f5313c8672205258d4d2483857bb
SevaErsh <erschidvs@yandex.ru>
2022-09-10 14:10:58 +0300

    add i18n and edit template

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
+from django.utils.translation import gettext as _
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'Users': _('Users'),
+        'SignUp': _('SignUp'),
+        'LogIn': _('LogIn'),
+    }
+    return render(request, 'index.html', context=context)
